bde3942edfb12bd54fe5e541561439e103371133
Ryan <49297331+USAFrenzy@users.noreply.github.com>
2022-12-01 19:01:48 -0500

    small update for seach box and just getting started with javascript for searching the data

---
+++ scripts/search/search_data.js
@@ -0,0 +1,17 @@
+
+// Don't really know what needs to actually be done here yet, just that it makes sense to programmatically approach
+// searching via regex matching against values in 'data' and displaying results that contain or are similar to the 
+// query being matched, as well as once the query has actually been submitted, redirecting to the linked page or 
+// displaying a generic 'search result not found' page. Given that C++ is my land of comfort, I'm hoping to pick up 
+// JS easily enough to do what I want here since JS has some vaguely similar paradigms.
+
+var data = {
+};
+
+function ParseSearchData(query) {
+
+}
+
+function DisplaySearchResults() {
+
+}
